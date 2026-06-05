1809ed4c3b7a0934c5ea301f1cb16b3bb6ea553b
chakrishabad-23 <122350525+chakrishabad-23@users.noreply.github.com>
2026-06-05 09:57:19 +0530

    commit 2

--- Python Expense Tracker/main.py
+++ Python Expense Tracker/main.py
@@ -1,30 +1,76 @@
 from expense import Expense
 from expense_manager import ExpenseManager
-class main:
-    while True:
-        print("\nExpense Tracker")
-        print("1. Add Expense")
-        print("2. View Expenses")
-        print("3.Summarize")
-        print("4. Exit")
-        option = input("Choose an option:  ")
-        if option == '1':
-            date = input("Enter date (YYYY-MM-DD): ")
-            category = input("Enter category: ")
-            amount = input("Enter amount: ")
-            description = input("Enter description: ")
-            expense = Expense(date, category, amount, description)
-            with open('expenses.csv', 'a') as file:
-                file.write(f"{expense.date},{expense.category},{expense.amount},{expense.description}\n")
-            print("Expense added successfully!")
-        elif option == '2':
-            exp_man = ExpenseManager()
-            exp_man.view_expense()
-        elif option == '3':
-            exp_man = ExpenseManager()
-            exp_man.summarize_expenses()
-        elif option == '4':
-            print("Exiting the program.")
-            break
-        else:
-            print("Invalid option. Please try again.")
+
+while True:
+
+    print("\nExpense Tracker")
+
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Total Summary")
+    print("4. Monthly Summary")
+    print("5. Category Summary")
+    print("6. Highest Spending Category")
+    print("7. Expense Pie Chart")
+    print("8. Spending Suggestion")
+    print("9. Exit")
+
+    option = input("Choose Option: ")
+
+    if option == '1':
+
+        date = input("Enter Date (YYYY-MM-DD): ")
+        category = input("Enter Category: ")
+        amount = input("Enter Amount: ")
+        description = input("Enter Description: ")
+
+        expense = Expense(
+            date,
+            category,
+            amount,
+            description
+        )
+
+        with open('expenses.csv', 'a') as file:
+            file.write(
+                f"{expense.date},{expense.category},{expense.amount},{expense.description}\n"
+            )
+
+        print("Expense Added Successfully")
+
+    elif option == '2':
+
+        ExpenseManager().view_expense()
+
+    elif option == '3':
+
+        ExpenseManager().summarize_expenses()
+
+    elif option == '4':
+
+        ExpenseManager().monthly_summary()
+
+    elif option == '5':
+
+        ExpenseManager().category_summary()
+
+    elif option == '6':
+
+        ExpenseManager().highest_spending_category()
+
+    elif option == '7':
+
+        ExpenseManager().pie_chart()
+
+    elif option == '8':
+
+        ExpenseManager().spending_suggestion()
+
+    elif option == '9':
+
+        print("Exiting...")
+        break
+
+    else:
+
+        print("Invalid Option")
